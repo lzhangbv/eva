45618a39e89126299726492bc7f0edda7e52391d
lzhangbv <lzhangbv@connect.ust.hk>
2022-10-07 23:38:15 +0800

    modified: vShampoo: precondtion tensors with layerwise-rescale

--- batch-cifar10.sh
+++ batch-cifar10.sh
@@ -18,22 +18,30 @@ dataset=cifar10
 #epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 kfac_name=eva kfac=0 fac=0 damping=0.03 stat_decay=$stat_decay kl_clip=$kl_clip warmup_epochs=5 lr_schedule=linear momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=7 ./train_cifar10.sh &
 #epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 kfac_name=eva kfac=1 fac=1 damping=0.03 stat_decay=$stat_decay kl_clip=$kl_clip warmup_epochs=5 lr_schedule=linear momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=8 ./train_cifar10.sh &
 #epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 kfac_name=eva kfac=0 fac=0 damping=0.03 stat_decay=$stat_decay kl_clip=$kl_clip warmup_epochs=5 lr_schedule=polynomial momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=9 ./train_cifar10.sh &
-#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 kfac_name=eva kfac=1 fac=1 damping=0.03 stat_decay=$stat_decay kl_clip=$kl_clip warmup_epochs=5 lr_schedule=polynomial momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=1 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 kfac_name=eva kfac=1 fac=1 damping=0.03 stat_decay=$stat_decay kl_clip=$kl_clip warmup_epochs=5 lr_schedule=polynomial momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=10 ./train_cifar10.sh &
 
 
 # (2) Shampoo vs. vectorized Shampoo
 bs=128 #128, 256, 512
 lr=0.1
+lrs=cosine
 epochs=100
 damping=0.03
 
 dnn=vgg19
 dataset=cifar10
-epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=step momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=6 ./train_cifar10.sh &
-#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=shampoo kfac=10 fac=10 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=step momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=2 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=6 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=shampoo kfac=10 fac=10 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=2 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=0.003 stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=7 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=0.3 stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=8 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=3 stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=9 ./train_cifar10.sh &
+
 
 dnn=resnet110
 dataset=cifar10
-#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=step momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=4 ./train_cifar10.sh &
-#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=shampoo kfac=10 fac=10 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=step momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=5 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=4 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=shampoo kfac=10 fac=10 damping=$damping stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=5 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=0.003 stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=2 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=0.3 stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=5 ./train_cifar10.sh &
+#epochs=$epochs dnn=$dnn dataset=$dataset batch_size=$bs base_lr=0.1 nworkers=4 opt_name=vshampoo kfac=1 fac=1 damping=3 stat_decay=1 kl_clip=1 warmup_epochs=5 lr_schedule=$lrs momentum=0.9 label_smoothing=0 cutmix=0 autoaugment=0 cutout=0 horovod=0 node_rank=10 ./train_cifar10.sh &
 
